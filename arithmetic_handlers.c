e3a3f330fd7411eb619358b88b9a590cead27cdd
John Gaitho <johngaitho05@gmail.com>
2023-08-24 16:52:54 +0300

    handle div, mul, mod

--- arithmetic_handlers.c
+++ arithmetic_handlers.c
@@ -58,18 +58,93 @@ void handle_sub(stack_t **stack, unsigned int line_number)
 
 	free(first);
 }
+
+/**
+ * handle_div - divide the second element by the top element
+ * @stack: a pointer to the stack
+ * @line_number: position of the opcode in the file
+ */
 void handle_div(stack_t **stack, unsigned int line_number)
 {
-	(void)stack;
-	(void)line_number;
+	int result;
+	stack_t *first, *second;
+
+	if (stack_len(*stack) < 2)
+	{
+		fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
+		free_stack(*stack);
+		exit(EXIT_FAILURE);
+	}
+
+	first = node_at_index(*stack, 0);
+	second = node_at_index(*stack, 1);
+	if (first->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line_number);
+		free_stack(*stack);
+		exit(EXIT_FAILURE);
+	}
+
+	result = second->n / first->n;
+
+	second->n = result;
+	(*stack) = second;
+
+	free(first);
 }
+
+/**
+ * handle_mul - multiplies the second element by first element
+ * @stack: a pointer to the stack
+ * @line_number: position of the opcode in the file
+ */
 void handle_mul(stack_t **stack, unsigned int line_number)
 {
-	(void)stack;
-	(void)line_number;
+	int result;
+	stack_t *first, *second;
+
+	if (stack_len(*stack) < 2)
+	{
+		fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
+		free_stack(*stack);
+		exit(EXIT_FAILURE);
+	}
+
+	first = node_at_index(*stack, 0);
+	second = node_at_index(*stack, 1);
+
+	result = second->n * first->n;
+
+	second->n = result;
+	(*stack) = second;
+
+	free(first);
 }
+
+/**
+ * handle_mod - find the remainder of dividing second element by first element
+ * @stack: a pointer to the stack
+ * @line_number: position of the opcode in the file
+ */
 void handle_mod(stack_t **stack, unsigned int line_number)
 {
-	(void)stack;
-	(void)line_number;
+	int result;
+	stack_t *first, *second;
+
+	if (stack_len(*stack) < 2)
+	{
+		fprintf(stderr, "L%d: can't mod, stack too short\n", line_number);
+		free_stack(*stack);
+		exit(EXIT_FAILURE);
+	}
+
+	first = node_at_index(*stack, 0);
+	second = node_at_index(*stack, 1);
+
+	result = second->n % first->n;
+
+	second->n = result;
+	(*stack) = second;
+
+	free(first);
 }
